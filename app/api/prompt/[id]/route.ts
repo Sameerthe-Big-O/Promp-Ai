55f1ac3798ebd0850a725d60ab78ce4d04ea72ea
Sameerthe-Big-O <diziaddict00@gmail.com>
2024-07-08 16:31:33 +0500

    adding

--- app/api/prompt/[id]/route.ts
+++ app/api/prompt/[id]/route.ts
@@ -3,81 +3,68 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
   req: NextRequest,
-  res: NextResponse,
-  {
-    params,
-  }: {
-    params: {
-      id: string;
-    };
-  }
+  { params }: { params: { id: string } }
 ) => {
   try {
-    console.log("id is here", params.id);
     const prompts = await prisma.prompt.findUnique({
       where: { id: params.id },
     });
 
     return NextResponse.json({ data: prompts }, { status: 200 });
   } catch (err) {
+    console.error(err);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
     );
   }
 };
+
 export const PATCH = async (
   req: NextRequest,
-  {
-    params,
-  }: {
-    params: {
-      id: string;
-    };
-  }
+  { params }: { params: { id: string } }
 ) => {
   try {
     const { prompt, tag } = await req.json();
 
     const prompts = await prisma.prompt.update({
       where: {
-        id: params.id as string,
+        id: params.id,
       },
       data: {
         prompt,
         tag,
       },
     });
+
     return NextResponse.json(
       { data: prompts, message: "Success" },
       { status: 200 }
     );
   } catch (err) {
+    console.error(err);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
     );
   }
 };
+
 export const DELETE = async (
-  res: NextResponse,
-  {
-    params,
-  }: {
-    params: {
-      id: string;
-    };
-  }
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) => {
   try {
     await prisma.prompt.delete({
       where: { id: params.id },
     });
+
     return NextResponse.json(
-      { message: "successfully deleted" },
+      { message: "Successfully deleted" },
       { status: 200 }
     );
   } catch (err) {
+    console.error(err);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
