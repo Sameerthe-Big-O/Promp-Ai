4f91b3941998c6e5cbddead9bae466199132d8a1
Sameerthe-Big-O <diziaddict00@gmail.com>
2024-07-30 18:30:02 +0500

    configure

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FaceProvider from "next-auth/providers/facebook";
 import prisma from "../../../../prisma/client";
-import { GoogleProfile, SessionDate } from "@/types/next-autth/types";
+
+console.log(prisma);
 const handler = NextAuth({
   providers: [
     GoogleProvider({
